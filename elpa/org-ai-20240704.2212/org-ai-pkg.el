e6998e5720bc6539db4dd9da2006c19a84437f8a
Buro Mookerji <353255+mookerji@users.noreply.github.com>
2024-09-07 10:22:16 -0700

    Revert "Updates for magit update."
    This reverts commit 1dd947027b53c4032e3d6a24b21a1d9e3431d797.

---
+++ elpa/org-ai-20240704.2212/org-ai-pkg.el
@@ -0,0 +1,13 @@
+(define-package "org-ai" "20240704.2212" "Use ChatGPT and other LLMs in org-mode and beyond"
+  '((emacs "27.1")
+    (websocket "1.15"))
+  :commit "5f8bf87aa5f6b7f685a3948481a5c6ab1ddc203c" :authors
+  '(("Robert Krahn" . "robert@kra.hn"))
+  :maintainers
+  '(("Robert Krahn" . "robert@kra.hn"))
+  :maintainer
+  '("Robert Krahn" . "robert@kra.hn")
+  :url "https://github.com/rksm/org-ai")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
